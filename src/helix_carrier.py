8c30fef5008dc991c075edd98be863af41bebccf
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-11-21 12:12:32 +0100

    helix carrier mesh

--- src/helix_carrier.py
+++ src/helix_carrier.py
@@ -3,28 +3,7 @@
 import pymesh, argparse
 import numpy as np
 from fix_mesh import fix_mesh
-
-def create_helix(nL, L, R, pitch, nR, RStart, REnd):
-    z = np.linspace(-L/2, L/2, nL)
-    theta = z * (2 * np.pi / pitch)
-    x = R * np.cos(theta)
-    y = R * np.sin(theta)
-    
-    vertices = np.transpose(np.vstack((x,y,z)))
-    edges = np.transpose(np.vstack((range(nL-1), range(1,nL))))
-    
-    wire_network = pymesh.wires.WireNetwork.create_from_data(vertices, edges)
-    
-    inflator = pymesh.wires.Inflator(wire_network)
-    
-    inflator.set_profile(nR)
-    inflator.set_refinement(2, "loop")
-    thickness = np.arange(wire_network.num_vertices) /  wire_network.num_vertices * (RStart-REnd) + REnd
-    inflator.inflate(thickness, per_vertex_thickness=True)
-
-    mesh = inflator.mesh
-
-    return mesh
+from helix import create_helix
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Create a helicoidal triangle mesh')
@@ -38,12 +17,22 @@ def parse_args():
     parser.add_argument('--smallRadiusStart', type=float, default = 0.5)
     parser.add_argument('--smallRadiusEnd', type=float, default = 0.5)
 
+    parser.add_argument('--sphereRadius', type=float, default = 2.0)
+
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    mesh = create_helix(args.nL, args.height, args.radius, args.pitch,
+    Hmesh = create_helix(args.nL, args.height, args.radius, args.pitch,
                         args.nR, args.smallRadiusStart, args.smallRadiusEnd)
+
+    R = args.sphereRadius
+    H = args.height
+    Smesh =  pymesh.generate_icosphere(R, [0.0, 0.0, H/2 + R * 0.7], refinement_order=2)
+
+    mesh = pymesh.boolean(Hmesh, Smesh,
+                          operation="union",
+                          engine="igl")
                         
     mesh = fix_mesh(mesh, "low")
 
