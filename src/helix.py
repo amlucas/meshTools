a27fbed1dd2592284dd2f122eb95d79e0de92d9d
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-11-21 11:48:15 +0100

    remesh

--- src/helix.py
+++ src/helix.py
@@ -2,21 +2,28 @@
 
 import pymesh, argparse
 import numpy as np
+from fix_mesh import fix_mesh
 
 parser = argparse.ArgumentParser(description='Create a helicoidal triangle mesh')
 
 parser.add_argument('--out', help='output file name', type=str)
+parser.add_argument('--pitch', type=float, default = 2.5)
+parser.add_argument('--height', type=float, default = 5.0)
+parser.add_argument('--nsegments', type=float, default = 32)
+parser.add_argument('--radius', type=float, default = 1.0)
+parser.add_argument('--smallRadius', type=float, default = 0.5)
 
 args = parser.parse_args()
 
-n = 32
-L0 = 5.0
-R0 = 0.5
-R = 1.0
-P = L0 / 3
 
-z = np.linspace(-L0/2, L0/2, n)
-theta = z * (2 * np.pi / P)
+n = args.nsegments
+L = args.height
+R = args.radius
+
+R0 = args.smallRadius
+
+z = np.linspace(-L/2, L/2, n)
+theta = z * (2 * np.pi / args.pitch)
 x = R * np.cos(theta)
 y = R * np.sin(theta)
 
@@ -28,8 +35,10 @@ wire_network = pymesh.wires.WireNetwork.create_from_data(vertices, edges)
 inflator = pymesh.wires.Inflator(wire_network)
 
 inflator.set_profile(6)
+inflator.set_refinement(2, "loop")
 inflator.inflate(R0, per_vertex_thickness=True)
 
 mesh = inflator.mesh
+mesh = fix_mesh(mesh, "low")
 
 pymesh.save_mesh(args.out, mesh)
