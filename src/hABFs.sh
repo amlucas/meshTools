f7e35b9d1a0c9bfc27e10729719d20f523fe9152
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2018-12-13 18:08:30 +0100

    minor

--- src/hABFs.sh
+++ src/hABFs.sh
@@ -1,2 +1,4 @@
+#! /bin/sh
+
 ./hybrid_ABF.py --out hABF_3.ply --radius 1.5 --smallRadius 0.7 --smallRadiusRatio 2.5
 ./hybrid_ABF.py --out hABF_6.ply --radius 3.0 --smallRadius 0.7 --smallRadiusRatio 2.5
